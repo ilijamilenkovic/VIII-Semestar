e99c33b59ef9ed1cd3effb9b8b2536b75fbc513c
Ilija Milenkovic <ilija.milenkovic@elfak.rs>
2023-03-07 12:18:54 +0100

    lab1 paralelni

--- Paralelni Sistemi/Lab/Lab 1/3.c
+++ Paralelni Sistemi/Lab/Lab 1/3.c
@@ -3,3 +3,139 @@ na  udaljenosti  i  od  glavne  dijagonale.  Proces  i  kreira  svoju  matricu  
 primljene  dijagonale  u  prvu  i  drugu  kolonu  matrice  a  ostala  mesta    popunjava  nulama. 
 Napisati MPI program koji realizuje opisanu komunikaciju korišćenjem izvedenih tipova 
 podataka i prikazuje vrednosti odgovarajućih kolona*/
+
+#include "mpi.h"
+#include "stdio.h"
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define n 6
+
+int main(int argc, char **argv){
+    srand(time(NULL));
+    MPI_Init(&argc, &argv);
+    int my_rank, size;
+    int block_count;
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int arr_of_dissplacements_top[size-1];
+    int arr_of_dissplacements_bottom[size-1];
+    int arr_of_block_lens[size-1];
+    int recv_arr[2*(size-1)];
+    
+    int mat[n][n];
+    //inicijalizacija
+    if(my_rank == 0){
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                mat[i][j] = rand() % 10;
+                printf("%d\t", mat[i][j]);
+            }
+            printf("\n");
+            
+        }
+
+        for (int i = 1; i < size; i++)
+        {
+            block_count = n-i;
+            int pom = 0;
+
+            
+            // printf("Proces %d arr_of_displacement:\n", i);
+            for (int j = 0; j < size-i; j++)
+            {
+                arr_of_dissplacements_top[j] = i + j * (n+1);
+                arr_of_dissplacements_bottom[j] = i * n + j*(n+1);
+                
+                arr_of_block_lens[pom++] = 1;
+            }
+            printf("Top:\n");
+            for (int j = 0; j < size-i; j++)
+            {
+                printf("%d ", arr_of_dissplacements_top[j]);
+            }
+
+            printf("\nBottom:\n");
+            for (int j = 0; j < size-i; j++)
+            {
+                printf("%d ", arr_of_dissplacements_bottom[j]);
+            }
+            
+
+            // printf("\n\n");
+            //gornja dijagonala
+            MPI_Datatype new_type;
+            MPI_Type_indexed(block_count, arr_of_block_lens, arr_of_dissplacements_top, MPI_INT, &new_type);
+            MPI_Type_commit(&new_type);
+            MPI_Send(mat, 1, new_type, i, 0, MPI_COMM_WORLD);
+
+
+            //donja dijagonala
+            MPI_Datatype new_type_bottom;
+            MPI_Type_indexed(block_count, arr_of_block_lens, arr_of_dissplacements_bottom, MPI_INT, &new_type_bottom);
+            MPI_Type_commit(&new_type_bottom);
+            MPI_Send(mat, 1, new_type_bottom, i, 0, MPI_COMM_WORLD);
+
+            
+        
+        }
+            
+    }
+    //ostali procesi
+    else{
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                mat[i][j] = 0;
+                
+            }
+            printf("\n");
+        }
+            
+        
+        MPI_Status status;
+        // MPI_Recv(recv_arr, n-my_rank, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        // MPI_Recv(&recv_arr[n-my_rank], n-my_rank, MPI_INT, 0,0, MPI_COMM_WORLD,&status );
+
+        MPI_Datatype new_type, new_type_resized;
+        MPI_Type_vector(n-my_rank,1,n,MPI_INT,&new_type);
+        MPI_Type_create_resized(new_type, 0, sizeof(int), &new_type_resized);
+        MPI_Type_commit(&new_type_resized);
+        MPI_Recv(&mat[0][0],1,new_type_resized,0,0,MPI_COMM_WORLD, &status);
+        MPI_Recv(&mat[0][1], 1, new_type_resized,0,0, MPI_COMM_WORLD, &status);
+
+
+        //NECE DA SE SINHRONIZUJE DA STAMPA KAKO TREBA
+        sleep(my_rank);
+        printf("Proces %d: \n",my_rank);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                
+                printf("%d \t", mat[i][j]);
+            }
+            
+
+        }  
+        printf("\n");          
+        
+
+        
+        
+
+    }
+
+    
+
+
+
+
+    MPI_Finalize();
+
+}
---
+++ Paralelni Sistemi/Lab/Lab 1/lab_zadatak_faks.c
@@ -0,0 +1,107 @@
+// 3. Napisati MPI program kojim se matrice Amxn i Bnxk, inicijalizovane u master procesu, množe i
+// rezultat se smešta u matricu C. Svakom procesu šalje se cela matrica A i po k/l kolona matrice B (l
+// je broj procesa, k je deljivo sa l) i svi procesi učestvuju u izračunavanju. Slanje k/l kolona matrice
+// B obavlja se odjednom, direktno iz matrice B. Matricu C prikazati iz master procesa. Zadatak
+// rešiti korišćenjem grupnih operacija i izvedenih tipova podataka.
+
+
+#include <mpi.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define m 3
+#define n 2
+#define k 6
+#define root 0
+
+int main(int argc, char **argv)
+{
+    srand(time(NULL)); 
+    MPI_Init(&argc, &argv);
+    int my_rank,size;
+    int A[m][n];
+    int B[n][k];
+    int C[m][k];
+    
+    
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int B_temp[n][k/size];
+    int C_temp[m][k/size];
+    //int recv_buffer_B[n * (k / size)];
+
+    if(my_rank == root){
+        //mat A init
+        printf("Matrica A: \n");
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){ 
+                A[i][j] = rand() % 10;
+                printf("%d\t", A[i][j]);
+            }
+            printf("\n");
+        }
+
+        //mat B init
+        printf("Matrica B: \n");
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < k; j++){ 
+                B[i][j] = rand() % 10;
+                printf("%d\t", B[i][j]);
+            }
+            printf("\n");
+        }
+    }
+
+    //Slanje cele matrice A
+    MPI_Bcast(A, m * n, MPI_INT, root, MPI_COMM_WORLD);
+
+    //Slanje po k/p kolona matrice B    
+    MPI_Datatype new_type, new_type_resized;
+    MPI_Type_vector(n, k / size, k, MPI_INT, &new_type);
+    MPI_Type_commit(&new_type);
+    MPI_Type_create_resized(new_type, 0, (k / size) * sizeof(int), &new_type_resized);
+    MPI_Type_commit(&new_type_resized);
+    
+    MPI_Scatter(B, 1, new_type_resized, B_temp,  n*(k/size), MPI_INT, root, MPI_COMM_WORLD);
+
+    //Izracunavanje:
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < k / size; j++)
+        {
+            C_temp[i][j] = 0;
+            for (int l = 0; l < n; l++)
+            {
+                C_temp[i][j] += (A[i][l]*B_temp[l][j]);
+            }
+            
+        }
+        
+    }
+     
+    MPI_Datatype return_type, return_type_resized;
+    MPI_Type_vector(m, k / size, k, MPI_INT, &return_type);
+    MPI_Type_commit(&return_type);
+    MPI_Type_create_resized(return_type, 0, (k / size) * sizeof(int), &return_type_resized);
+    MPI_Type_commit(&return_type_resized);
+
+    MPI_Gather(C_temp, m*(k/size), MPI_INT, C, 1, return_type_resized, root, MPI_COMM_WORLD);
+
+
+    if(my_rank == root){
+    printf("Rezultat:\n-----------------\n");
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            printf("%d\t", C[i][j]);
+        }
+        
+        printf("\n");
+    }
+    }
+
+    MPI_Finalize();
+
+}
